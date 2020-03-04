e68da3b26726826f970711e16957bd69f96e7692
Alec Barber <barberal@tcd.ie>
2020-03-04 13:16:12 +0000

    fixed algo_eval

--- algo_eval.py
+++ algo_eval.py
@@ -3,7 +3,7 @@ This is the file that all evaluations will take place, ideally we should
 not have to access Othello.py again :)
 """
 from Othello import othello
-from human_ai_example import human_ai
-from 
+from othello_ai import human_ai, minimax_ai
+
 game = othello()
 game.startgame(human_ai())
