84b4b257ac1674c254b3166115033eeaebc054f0
Alec Barber <barberal@tcd.ie>
2020-03-07 13:26:37 +0000

    fogot this

--- algo_eval.py
+++ algo_eval.py
@@ -4,11 +4,16 @@ not have to access Othello.py again :)
 """
 from Othello import othello
 from othello_ai import human_ai, decisionRule_ai, minimax_ai, NN_ai
+from Othello_Evaluation import othello_eval
 
 # Create new instance of othello game with spefied ai players
-#game = othello(human_ai("X"), decisionRule_ai("O"))
-#game = othello(human_ai("X"), human_ai("O"))
+# game = othello(human_ai("X"), decisionRule_ai("O"))
+# game = othello(human_ai("X"), human_ai("O"))
 game = othello(human_ai(1), decisionRule_ai(-1))
 
 # Begin game
-score = game.startgame(start_move=5)
+# score = game.startgame(start_move=5)
+
+evaluation = othello_eval(decisionRule_ai(1), decisionRule_ai(-1), runs=10)
+evaluation.gameStartEval()
+evaluation.plotGameStartResults()
